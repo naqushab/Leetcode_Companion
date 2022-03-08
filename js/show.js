d2d2e70ba1a145e5a78bce8a2c9938f4438b151f
Naqushab Neyazee <neyazee@adobe.com>
2022-03-08 06:09:41 +0530

    added new data and updated url

--- js/show.js
+++ js/show.js
@@ -1,25 +1,25 @@
-d3.csv("http://localhost:8000/data/leetcode.csv", function(d) {
+d3.csv("https://raw.githubusercontent.com/naqushab/Leetcode_Companion/develop/data/leetcode.csv", function(d) {
   return {
-  	no : +d.No,
+    no : +d.No,
     id : +d.ID,
     title : d.Title,
     url : d.Link,
     difficulty : d.Difficulty
   };
 }).then(function(data) {
-  var random = d3.randomInt(100)()
+  var random = d3.randomInt(data.length)()
   var container = d3.select("div")
-    .data(data.filter(function(d){ return d.no === random}))
+                    .data(data.filter(function(d){ return d.no === random}))
 
   var title = container.select(".title")
-                .append("p")
-                .text(function(d){return d.id + ". " + d.title})
+                       .append("p")
+                       .text(function(d){return d.id + ". " + d.title})
 
   var difficulty = container.select(".diff")
-                    .text(function(d){return d.difficulty})
+                            .text(function(d){return d.difficulty})
 
   var button = container.select("#doit")
-                  .on("click", function(d){
-                    window.open(d.url)
-                  })
+                        .on("click", function(d){
+                          window.open(d.url)
+                        })
 });
